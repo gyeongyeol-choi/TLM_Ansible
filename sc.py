c260dae844b60a099ad976691638f819e6272fe6
최경열 <gyeongyeol_choi@tmax.co.kr>
2018-03-28 10:59:26 +0900

    change the key name of json

--- sc.py
+++ sc.py
@@ -1,30 +1,27 @@
-#!/usr/bin/python3
 import os
 import sys
+import subprocess
 import json
 import ansible
 
 
 host_ip = "192.168.27.52"
 pkg_list = []
-cnt=0
-#f = open("/home/tmax/test_dir/"+host_ip+"/pkg_list_ins.txt")
-f = open("/home/tmax/test_dir/"+host_ip+"/pkg_list_rep.txt")
-#f = open("/home/tmax/test_dir/"+host_ip+"/pkg_list_upg.txt")
 
+p = subprocess.call('ansible-playbook playbook/get_list_ins.yml', shell=True)
 
-
+f = open("../test_dir/"+host_ip+"/pkg_list_ins.txt")
+#f = open("../test_dir/"+host_ip+"/pkg_list_rep.txt")
+#f = open("../test_dir/"+host_ip+"/pkg_list_upg.txt")
 
 while 1:
-	line = f.readline().replace("\n", "")
-	if not line: break
-	
-	pkg_list.append(line)
+        line = f.readline().replace("\n", "")
+        if not line: break
 
-	cnt = cnt + 1
+        pkg_list.append(line)
 
-dic = {"ip": host_ip, "pkg": pkg_list}
-#print(dic)
+
+dic = {"nodeID": host_ip, "packName": pkg_list}
 
 jsonString = json.dumps(dic, indent=4)
 print(jsonString)

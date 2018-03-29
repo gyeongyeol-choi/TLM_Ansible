1ace51344ea2a47fce899cf4f577b4646a1ffebc
최경열 <gyeongyeol_choi@tmax.co.kr>
2018-03-29 09:48:44 +0900

    add the process to handle args (playbook, ip etc..)

--- sc.py
+++ sc.py
@@ -4,24 +4,75 @@ import subprocess
 import json
 import ansible
 
+def usage():
+	print("""**********************************************************************************************************************
+Usage : python %s [-p play] [-i host_ip]
+* play: get_ins (Make the Installed package list) / get_rep (Make the package list in the repository) / ins (Install the selected package(s)) / del (Remove the selected package(s))
+* host_ip : ipv4 address of the remote node
+**********************************************************************************************************************""" % (sys.argv[0]))
 
-host_ip = "192.168.27.52"
-pkg_list = []
+#play=""
+#pl_path=""
 
-p = subprocess.call('ansible-playbook playbook/get_list_ins.yml', shell=True)
+def configuration():
+	global play
+	global host_ip
 
-f = open("../test_dir/"+host_ip+"/pkg_list_ins.txt")
-#f = open("../test_dir/"+host_ip+"/pkg_list_rep.txt")
-#f = open("../test_dir/"+host_ip+"/pkg_list_upg.txt")
+	if sys.argv.count('-h') == 1:
+		usage()
+		sys.exit()
+	if sys.argv.count('-p') == 1:
+		index = sys.argv.index('-p') + 1
+		play = sys.argv[index]
+	else:
+		print "Selecting the default playbook option (get_ins)"
+		play = 'get_ins'
 
-while 1:
-        line = f.readline().replace("\n", "")
-        if not line: break
+	if sys.argv.count('-i') == 1:
+		index = sys.argv.index('-i') + 1
+		host_ip = sys.argv[index]
+	else:
+		print "Using the default host ip (192.168.27.52)"
+		host_ip = "192.168.27.52"
 
-        pkg_list.append(line)
+def playbook():
+	global pl_path # Playbook Path
+
+	if play == 'get_ins':
+		pl_path = 'playbook/get_list_ins.yml'
+	elif play == 'get_rep':
+		pl_path = 'playbook/get_list_rep.yml'
+	elif play == 'ins':
+		pl_path = "playbook/install_pkg.yml --extra-value='@../files/install.json'"
+	elif play == 'del':
+		pl_path = "playbook/delete_pkg.yml --extra-value='@../files/delete.json'"
+	else:
+		print ("Error!!: Invalid argument (-p) ...")
+		sys.exit()
 
 
-dic = {"nodeID": host_ip, "packName": pkg_list}
+if __name__ == "__main__":
+	
+	pkg_list = []
+	
+	configuration()
+	print("Play: " + play)
+	playbook()
 
-jsonString = json.dumps(dic, indent=4)
-print(jsonString)
+	p = subprocess.call('ansible-playbook '+pl_path, shell=True)
+
+	if play == 'get_ins':
+		f = open("../test_dir/"+host_ip+"/pkg_list_ins.txt")
+	elif play == 'get_rep':
+		f = open("../test_dir/"+host_ip+"/pkg_list_rep.txt")
+
+	while 1:
+	        line = f.readline().replace("\n", "")
+	        if not line: break
+
+	        pkg_list.append(line)
+
+	dic = {"nodeID": host_ip, "packName": pkg_list}
+
+	jsonString = json.dumps(dic, indent=4)
+	print(jsonString)

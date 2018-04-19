56651900b8fa45ff74a58a07be9548bbdcee0820
최경열 <gyeongyeol_choi@tmax.co.kr>
2018-04-19 15:10:58 +0900

    minor change

--- files/get_list_rep.sh
+++ files/get_list_rep.sh
@@ -1,5 +1,5 @@
 # Verify the package by setting the key
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 9011567A260F6FE7
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 9011567A260F6FE7
     
 # Construct the package list (in repository)
-sudo apt list | cut -d '/' -f1  | sed '1d' > '/home/po7/SC_PKG_LIST/pkg_list_rep.txt'
+apt list | cut -d '/' -f1  | sed '1d' > '/home/po7/SC_PKG_LIST/pkg_list_rep.txt'

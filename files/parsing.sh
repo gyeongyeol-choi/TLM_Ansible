6efe7be496353ba78511cbab90cd2c124dbcdc7b
최경열 <gyeongyeol_choi@tmax.co.kr>
2018-05-14 12:59:59 +0900

    implement the install / delete for multiple hosts

--- files/parsing.sh
+++ files/parsing.sh
@@ -4,11 +4,12 @@ while read line
 do 
 	if [[ "$line" =~ "The following packages will be upgraded:" ]];then
 		flag=1
+		continue
 	fi
 	if [[ "$line" =~ "not upgraded." ]];then
 		flag=0
 	fi
 	if [[ $flag == 1 ]];then
-		echo $line
+		echo $line | sed -e 's/ /\n/g'
 	fi
 done < "pkg_list_upg_pre.txt"
